c8936c0187eae45faccc6fda20275cb42e894c60
AlexisBustamante <146131446+Alexis-busta74@users.noreply.github.com>
2026-04-22 01:34:46 -0300

    Add files via upload

---
+++ datos.js
@@ -0,0 +1,72 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Açaí – Vitalidad y protección",
+        precio: 1500,
+        descripcion: "El sahumerio de Açaí activa la energía vital, protege el ambiente y eleva la vibración. Ideal para limpiezas energéticas, meditación y rituales de renovación. Su aroma frutal y suave conecta con la fuerza de la naturaleza amazónica, aportando equilibrio, alegría y bienestar interior.",
+        fotos: ["./imagenes/3.png", "./imagenes/4.png"]
+    },
+    {
+        id: 2,
+        nombre: "Açaí & Sándalo - Energía vital y conexión interior.",
+        precio: 1800,
+        descripcion: "El açaí y el sándalo se encuentran para crear un sahumerio que equilibra la energía y aquieta el alma. La vitalidad del açaí despierta y renueva, mientras la madera de sándalo envuelve con su aroma cálido, profundo y sereno. Juntos armonizan el ambiente, invitan a la calma consciente y acompañan momentos de conexión interior, limpieza energética y meditación. Un equilibrio perfecto entre fuerza natural y paz interior",
+        fotos: ["./imagenes/5.png", "./imagenes/6.png"]
+    },
+    {
+        id: 3,
+        nombre: "Nag Champa – Armonía y calma",
+        precio: 1500,
+        descripcion: "El sahumerio de Nag Champa, originario de la India, se elabora a partir de flores y resinas naturales. Es ideal para armonizar el ambiente y aquietar la mente. Su aroma suave y envolvente favorece la calma emocional, la concentración y el equilibrio interior. Tradicionalmente utilizado en templos y espacios de meditación por su capacidad de crear atmósferas de paz profunda. En la actualidad sus principales usos son para realizar meditación, yoga, relajación, estudio y búsqueda de paz interior.",
+        fotos: ["./imagenes/7.png", "./imagenes/8.png"]
+    },
+    {
+        id: 4,
+        nombre: "Palo Santo – Limpieza y armonía",
+        precio: 1500,
+        descripcion: "El sahumerio de Palo Santo proviene de la madera de un árbol sagrado sudamericano. Es ideal para limpiar y armonizar energéticamente los espacios. Su aroma amaderado genera bienestar y protección. Su uso ancestral lo convierte en un aliado para renovar la energía y comenzar de nuevo. Actualmente se utiliza para realizar limpiezas energéticas del hogar, protección, renovación y nuevos comienzos.",
+        fotos: ["./imagenes/9.png", "./imagenes/10.png"]
+    },
+    {
+        id: 5,
+        nombre: "Jazmín – Alegría y amor",
+        precio: 1500,
+        descripcion: "El sahumerio de Jazmín se obtiene de las flores de esta planta aromática. Es ideal para elevar el ánimo y armonizar las emociones. Su fragancia floral aporta alegría y suavidad emocional. Asociado simbólicamente al amor, la sensibilidad y la energía femenina. Su uso principal es para el bienestar emocional, amor propio, armonía en vínculos y ambientes del hogar.",
+        fotos: ["./imagenes/11.png", "./imagenes/12.png"]
+    },
+    {
+        id: 6,
+        nombre: "Coco & Vainilla – Calma y Dulzura",
+        precio: 1500,
+        descripcion: "El sahumerio de Jazmín se obtiene de las flores de esta planta aromática. Es ideal para elevar el ánimo y armonizar las emociones. Su fragancia floral aporta alegría y suavidad emocional. Asociado simbólicamente al amor, la sensibilidad y la energía femenina. Su uso principal es para el bienestar emocional, amor propio, armonía en vínculos y ambientes del hogar.",
+        fotos: ["./imagenes/13.png", "./imagenes/14.png"]
+    },
+    {
+        id: 7,
+        nombre: "Incienso – Limpieza y elevación",
+        precio: 1500,
+        descripcion: "El sahumerio de Incienso proviene de la resina de árboles del género Boswellia, utilizados desde la antigüedad en rituales sagrados. Es ideal para purificar el ambiente y elevar la energía. Su aroma profundo y envolvente limpia espacios y acompaña la conexión espiritual. Considerado un puente entre lo terrenal y lo espiritual. Ideal para limpieza energética, oración, rituales espirituales y momentos de reflexión.",
+        fotos: ["./imagenes/15.png", "./imagenes/16.png"]
+    },
+    {
+        id: 8,
+        nombre: "Citronela – Limpieza y Protección",
+        precio: 1500,
+        descripcion: "El sahumerio de Citronela, obtenido de hojas aromáticas de una planta herbácea tropical, es ideal para limpiar y refrescar el ambiente. Su aroma cítrico y vibrante ayuda a disipar energías densas, aportar claridad y generar una sensación de orden y protección. Además de su uso energético, es conocida por ayudar a ahuyentar mosquitos de forma natural. Ideal para limpieza energética, protección del hogar, renovación de ambientes, espacios abiertos y momentos donde se busca frescura, claridad y bienestar.",
+        fotos: ["./imagenes/17.png", "./imagenes/18.png"]
+    },
+    {
+        id: 9,
+        nombre: "Mirra – Protección Profunda",
+        precio: 1500,
+        descripcion: "El sahumerio de Mirra proviene de la resina aromática de un árbol originario del noreste de África y la península arábiga. Su aroma cálido, profundo y ligeramente especiado es ideal para la limpieza energética profunda y la protección del ambiente. Favorece la introspección, la sanación emocional y la conexión espiritual. Utilizada desde la antigüedad en rituales sagrados, simboliza purificación, fortaleza interior y transformación. Sus principales usos son para la limpieza profunda, protección energética, sanación, cierre de ciclos y meditación introspectiva.",
+        fotos: ["./imagenes/19.png", "./imagenes/20.png"]
+    },
+    {
+        id: 10,
+        nombre: "Lavanda - Relajación y equilibrio para tu hogar",
+        precio: 1500,
+        descripcion: "La lavanda es una planta aromática del Mediterráneo usada desde la antigüedad, especialmente por los romanos, por su perfume y efectos relajantes. Su aroma ayuda a reducir el estrés, calmar la mente y generar bienestar. Es ideal para relajarse, dormir mejor y crear ambientes tranquilos.",
+        fotos: ["./imagenes/21.png", "./imagenes/22.png"]
+    }
+];
